7dd983f680b0046e35722259d7ea97a8cdd81ed2
finn von friesland <finn.von.friesland@googlemail.com>
2014-10-27 11:34:53 +0100

    minor improvement for setup

--- lib/lifelines.rb
+++ lib/lifelines.rb
@@ -12,15 +12,17 @@ require 'lifelines/setup'
 require 'lifelines/teardown'
 require 'lifelines/error_logging'
 
+$arg = ARGV[0]
+$all_arg = ARGV.join(" ")
+
+unless $arg == 'firefox' or $arg == 'headless' or $arg == 'chrome'
+  abort ("you did not choose a correct browser. please choose 'firefox' or 'headless' or 'chrome'")
+end
+
+
 def load_file(f)
   Dir[File.dirname(__FILE__) + "#{f}/*.rb"].each { |file| require file }
 end
-
-unless ARGV[0] == 'firefox' or ARGV[0] == 'headless' or ARGV[0] == 'chrome'
-  abort ("you did not choose a correct browser. please choose 'firefox' or 'headless' or 'chrome'")
-end
-$arg = ARGV[0]
-$all_arg = ARGV.join(" ")
 
 load_file('/classes/**')
 load_file('/tests/**')
